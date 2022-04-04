b1c7de2d16031992bd8ed3a8d0c6d8d114d31290
realdiganta <digantakalita.ai@gmail.com>
2022-04-05 00:57:30 +0530

    contract done

---
+++ src/RockPaperScissors.sol
@@ -0,0 +1,163 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+/**
+    First a game will be created with the addresses of each player, bet amount and the token
+    Then each player will buy chips by sending some amount of token to bet in the game
+    On start of a game, the first player will first send a hashed version of his hand(rocks, papers or scissors)
+    Then the second player will play his hand openly
+    Then the first player will reveal his hand
+    The contract will first verify that his hand is correct based on the hash that she submitted before 
+    Once verified, the contract will send the bet amount chips to whoever won that round
+ */
+
+contract RockPaperScissors {
+    enum Action {
+        Rock,
+        Paper,
+        Scissors
+    }
+
+    enum GameState {
+        InActive, // once game is finished, it will go to inactive state
+        Active, // once player1 submits hash entry it will go to active state
+        // and then once player2 submits game will go to finished state, waiting for player1 to reveal hash and decide winner
+        Finished // players cannot bet if the game is in a finished state, you must wait till the game is in Inactive state again, which will happen after the bet amount is rewarded to the winner
+    }
+
+    struct Game {
+        uint256 round;
+        GameState state; // current state of the game
+        address player1;
+        address player2;
+        uint256 betAmount;
+        IERC20 token;
+    }
+
+    struct Hands {
+        bytes32 player1; // the hashed entry of player 1 (rocks, paper or scissors)
+        Action player2; // unhashed entry of player 2
+    }
+
+    event GameCreated(Game game);
+
+    uint256 public totalGames;
+    // gameId => Game
+    mapping(uint256 => Game) public games;
+    // player => gameId => number of chips
+    mapping(address => mapping(uint256 => uint256)) public chips;
+
+    // gameId => roundId => Hands
+    mapping(uint256 => mapping(uint256 => Hands)) public hands;
+
+    // player1_hand => player2_hand => index of the winner (0 for player1 & 1 for player2)
+    mapping(Action => mapping(Action => uint256)) private _judge;
+
+    constructor() {
+        _judge[Action.Rock][Action.Paper] = 1;
+        _judge[Action.Rock][Action.Scissors] = 0;
+        _judge[Action.Paper][Action.Rock] = 0;
+        _judge[Action.Paper][Action.Scissors] = 1;
+        _judge[Action.Scissors][Action.Rock] = 1;
+        _judge[Action.Scissors][Action.Paper] = 0;
+    }
+
+    function createGame(
+        address _player1,
+        address _player2,
+        uint256 _betAmount,
+        address _token
+    ) external {
+        games[totalGames] = Game({
+            round: 0,
+            state: GameState.InActive,
+            player1: _player1,
+            player2: _player2,
+            betAmount: _betAmount,
+            token: IERC20(_token)
+        });
+        emit GameCreated(games[totalGames]);
+
+        totalGames += 1;
+    }
+
+    /**
+        @dev once a game is created, the players must call this method to buy chips for that game that
+        they will use to bet 
+     */
+    function buyChips(
+        address _for,
+        uint256 _gameId,
+        uint256 _amount
+    ) public {
+        Game memory game = games[_gameId];
+
+        require(
+            IERC20(game.token).transferFrom(msg.sender, address(this), _amount)
+        );
+
+        chips[_for][_gameId] += _amount;
+    }
+
+    /**
+        called by a player1 to play rock, paper or scissors
+        player1 sends a hashed entry with a secret key to the game
+     */
+    function playPlayer1(uint256 _gameId, bytes32 _hand) external {
+        Game storage game = games[_gameId];
+
+        require(game.state == GameState.InActive, "Error: Not your turn");
+        require(msg.sender == game.player1, "Error: Wrong sender");
+
+        chips[msg.sender][_gameId] -= game.betAmount;
+        hands[_gameId][game.round].player1 = _hand; // saved for later proof when the hand is revealed
+        game.state = GameState.Active;
+    }
+
+    function playPlayer2(uint256 _gameId, Action _hand) external {
+        Game storage game = games[_gameId];
+
+        require(game.state == GameState.Active, "Error: Not your turn");
+        require(msg.sender == game.player2, "Error: Wrong sender");
+
+        chips[msg.sender][_gameId] -= game.betAmount;
+        hands[_gameId][game.round].player2 = _hand;
+        game.state = GameState.Finished;
+    }
+
+    /**
+        @dev called by player1 with his hand revealed to decide the winner
+     */
+    function showdown(
+        uint256 _gameId,
+        uint256 _secret,
+        Action _hand
+    ) external {
+        Game storage game = games[_gameId];
+
+        require(game.state == GameState.Finished, "Error: game not finished");
+        require(msg.sender == game.player1, "Error: Wrong sender");
+
+        // verify player1's card
+        bytes32 actualHash = keccak256(abi.encodePacked(_secret, _hand));
+        require(
+            actualHash == hands[_gameId][game.round].player1,
+            "Error: Cannot verify hash"
+        );
+
+        // decide the winner
+        uint256 winnerIndex = _judge[_hand][hands[_gameId][game.round].player2];
+
+        if (winnerIndex == 0) {
+            chips[game.player1][_gameId] += 2 * game.betAmount;
+        } else {
+            chips[game.player2][_gameId] += 2 * game.betAmount;
+        }
+
+        // re initialize the game
+        game.round += 1;
+        game.state = GameState.InActive;
+    }
+}
